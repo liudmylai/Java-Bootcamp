120d17c7c9657094cb44597b98f8eb461a14f208
liudmylai <liudmyla.iovenko@gmail.com>
2022-02-18 22:15:05 -0500

    update logic

--- module-1/hangman/Hangman.java
+++ module-1/hangman/Hangman.java
@@ -6,70 +6,70 @@ public class Hangman {
     static Scanner scan = new Scanner(System.in);
 
     public static String[] words = {"ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
-    "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer",
-    "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat",
-    "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose",
-    "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", 
-    "python", "rabbit", "ram", "rat", "raven","rhino", "salmon", "seal",
-    "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan",
-    "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf",
-    "wombat", "zebra"};
+                                    "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer",
+                                    "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat",
+                                    "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose",
+                                    "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", 
+                                    "python", "rabbit", "ram", "rat", "raven","rhino", "salmon", "seal",
+                                    "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan",
+                                    "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf",
+                                    "wombat", "zebra"};
 
     public static String[] gallows = {"+---+\n" +
-    "|   |\n" +
-    "    |\n" +
-    "    |\n" +
-    "    |\n" +
-    "    |\n" +
-    "=========\n",
+                                      "|   |\n" +
+                                      "    |\n" +
+                                      "    |\n" +
+                                      "    |\n" +
+                                      "    |\n" +
+                                      "=========\n",
 
-    "+---+\n" +
-    "|   |\n" +
-    "O   |\n" +
-    "    |\n" +
-    "    |\n" +
-    "    |\n" +
-    "=========\n",
+                                      "+---+\n" +
+                                      "|   |\n" +
+                                      "O   |\n" +
+                                      "    |\n" +
+                                      "    |\n" +
+                                      "    |\n" +
+                                      "=========\n",
 
-    "+---+\n" +
-    "|   |\n" +
-    "O   |\n" +
-    "|   |\n" +
-    "    |\n" +
-    "    |\n" +
-    "=========\n",
+                                      "+---+\n" +
+                                      "|   |\n" +
+                                      "O   |\n" +
+                                      "|   |\n" +
+                                      "    |\n" +
+                                      "    |\n" +
+                                      "=========\n",
 
-    " +---+\n" +
-    " |   |\n" +
-    " O   |\n" +
-    "/|   |\n" +
-    "     |\n" +
-    "     |\n" +
-    " =========\n",
+                                      " +---+\n" +
+                                      " |   |\n" +
+                                      " O   |\n" +
+                                      "/|   |\n" +
+                                      "     |\n" +
+                                      "     |\n" +
+                                      " =========\n",
 
-    " +---+\n" +
-    " |   |\n" +
-    " O   |\n" +
-    "/|\\  |\n" + //if you were wondering, the only way to print '\' is with a trailing escape character, which also happens to be '\'
-    "     |\n" +
-    "     |\n" +
-    " =========\n",
+                                      " +---+\n" +
+                                      " |   |\n" +
+                                      " O   |\n" +
+                                      "/|\\  |\n" + 
+                                      "     |\n" +
+                                      "     |\n" +
+                                      " =========\n",
 
-    " +---+\n" +
-    " |   |\n" +
-    " O   |\n" +
-    "/|\\  |\n" +
-    "/    |\n" +
-    "     |\n" +
-    " =========\n",
+                                      " +---+\n" +
+                                      " |   |\n" +
+                                      " O   |\n" +
+                                      "/|\\  |\n" +
+                                      "/    |\n" +
+                                      "     |\n" +
+                                      " =========\n",
 
-    " +---+\n" +
-    " |   |\n" +
-    " O   |\n" +
-    "/|\\  |\n" + 
-    "/ \\  |\n" +
-    "     |\n" +
-    " =========\n"};
+                                      " +---+\n" +
+                                      " |   |\n" +
+                                      " O   |\n" +
+                                      "/|\\  |\n" + 
+                                      "/ \\  |\n" +
+                                      "     |\n" +
+                                      " =========\n"};
    
     /**
      * Function name: randomWord
@@ -102,19 +102,47 @@ public class Hangman {
             hidenWord[i] = '_';
         }
 
+        while(counter < 6) {
+            System.out.println(gallows[counter]);
 
-
-        System.out.println(gallows[counter]);
-        System.out.print("Word:    ");
-        for (char i : hidenWord) {
-            System.out.print(i + " ");
+            System.out.print("Word:    ");
+            for (char i : hidenWord) {
+                System.out.print(i + " ");
+            }
+            System.out.println("\n");
+            System.out.println("Misses: " + misses + "\n");
+            System.out.print("Guess: ");
+            guess = scan.next().charAt(0);
+            boolean isMatched = false;
+            for (int i = 0; i < clueWord.length; i++) {
+                if (guess == clueWord[i]) {
+                    hidenWord[i] = guess;
+                    isMatched = true;
+                }
+            }
+            if (isMatched) {
+                if (Arrays.equals(clueWord, hidenWord)) {
+                    break;
+                }
+            } else {
+                counter++;
+                misses += guess;
+            }
         }
-        System.out.println("\n");
-        System.out.println("Misses: " + misses + "\n");
-        System.out.print("Guess: ");
-        guess = scan.next().charAt(0);
 
-
+        if(counter < 6) {
+            System.out.println(gallows[counter]);
+            System.out.print("Word:    ");
+            for (char i : hidenWord) {
+                System.out.print(i + " ");
+            }
+            System.out.println("\n");
+            System.out.println("GOOD WORK!");
+        } else {
+            System.out.println(gallows[counter]);
+            System.out.println("R.I.P.\n");
+            System.out.println("The word was: '" + word + "'");
+        }
 
         scan.close();
     }
