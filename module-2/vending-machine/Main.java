312c99b0d0799ced967ecffa4273b33cbed2860a
liudmylai <liudmyla.iovenko@gmail.com>
2022-02-20 20:57:14 -0500

    initial commit

---
+++ module-2/vending-machine/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("\t************************************************");
+        System.out.println("\t             WELCOME TO JAVA DRINKS!            ");
+        System.out.println("\t************************************************");
+        
+    }
+}
