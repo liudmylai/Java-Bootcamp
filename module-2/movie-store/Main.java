5b5d802c83ae44be300e666c99ad6090cc3fda73
liudmylai <liudmyla.iovenko@gmail.com>
2022-03-14 15:41:09 -0400

    added class Store with functionality

--- module-2/movie-store/Main.java
+++ module-2/movie-store/Main.java
@@ -1,6 +1,24 @@
+import models.*;
+
 public class Main {
         public static void main(String[] args) {
-            Movie movie = new Movie("Gilty", "DVD", 5);
-            System.out.println(movie);
+            // Movie movie = new Movie("Gilty", "DVD", 5);
+            // System.out.println(movie);
+            Store store = new Store();
+            Movie movie1 = new Movie("The Shawshank Redemption", "Blue-Ray", 9.2);
+            Movie movie2 = new Movie("The Godfather", "Blue-Ray", 9.1);
+            Movie movie3 = new Movie("The Godfather: Part II", "DVD", 9.0);
+
+            store.addMovie(movie1);
+            store.addMovie(movie2);
+            store.addMovie(movie3);
+
+            store.action("The Godfather", "sell");  
+            store.action("The Shawshank Redemption", "rent"); 
+            store.action("The Shawshank Redemption", "return"); 
+
+            System.out.println(store);
+
+            
         }
 }
