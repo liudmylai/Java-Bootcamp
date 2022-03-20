728f21977bc68d28e3d71b860ca82e6ddba6b30b
liudmylai <liudmyla.iovenko@gmail.com>
2022-03-19 20:22:54 -0400

    stream operations practice

---
+++ module-2/unit-testing/streams-starter/Filter.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Filter {
+
+    static ArrayList<Double> prices = new ArrayList<Double>();
+
+
+    public static void main(String[] args) {
+        prices.add(1.99);
+        prices.add(4.99);
+        prices.add(10.99);
+        prices.add(15.99);
+
+        prices.stream()
+            .filter((price) -> price < 5)
+            .forEach((price)-> System.out.println(price));
+
+
+    }
+
+ 
+
+}
