5ead4e725231e806df981c69ca8f6961ca69e984
liudmylai <liudmyla.iovenko@gmail.com>
2022-04-03 18:50:52 -0400

    added createObject()

--- module-2/bank-management/Main.java
+++ module-2/bank-management/Main.java
@@ -10,7 +10,7 @@ public class Main {
     static Bank bank = new Bank();
 
     public static void main(String[] args) {
-
+    
 
     }
 
@@ -29,5 +29,23 @@ public class Main {
              System.out.println(e.getMessage());
          }
      }
+    /**
+     * Name: createObject
+     * @param values (String[] values)
+     * @return Account
+     * 
+     * Inside the function:
+     *   1. Dynamically creates a Chequing, Loan, or Savings object based on the values array. 
+     */
+    public static Account createObject(String[] values) {
+            try {
+            return (Account)Class.forName("models." + values[0])
+            .getConstructor(String.class, String.class, double.class)
+            .newInstance(values[1], values[2], Double.parseDouble(values[3]));
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
 
 }
