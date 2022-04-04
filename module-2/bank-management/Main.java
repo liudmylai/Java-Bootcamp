b56af10ba2067be5d66c98748b8232679c33467d
liudmylai <liudmyla.iovenko@gmail.com>
2022-04-04 15:37:35 -0400

    update logic

--- module-2/bank-management/Main.java
+++ module-2/bank-management/Main.java
@@ -1,6 +1,7 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
@@ -18,7 +19,15 @@ public class Main {
             ArrayList<Account> accounts = returnAccounts();
             loadAccounts(accounts);
 
-        } catch (FileNotFoundException e) {
+            ArrayList<Transaction> transactions = returnTransactions();
+            runTransactions(transactions);
+            bank.deductTaxes();
+            for (Account account : accounts) {
+                System.out.println("\n\t\t\t\t\t ACCOUNT\n\n\t"+ account +"\n\n");
+                transactionHistory(account.getId());
+            }
+            
+         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
     }
@@ -69,7 +78,7 @@ public class Main {
      */
     public static ArrayList<Account> returnAccounts() throws FileNotFoundException {
         ArrayList<Account> accounts = new ArrayList<Account>();
-        FileInputStream fis = new FileInputStream("data/accounts.txt");
+        FileInputStream fis = new FileInputStream(ACCOUNTS_FILE);
         Scanner scan = new Scanner(fis);
         while (scan.hasNextLine()) {
             String line = scan.nextLine();
@@ -91,4 +100,58 @@ public class Main {
         accounts.stream().forEach(account -> bank.addAccount(account));
     }
 
+    /**
+     * Name: returnTransactions()
+     * @return ArrayList<Transaction>
+     * @throws FileNotFoundException
+     * 
+     * Inside the function:
+     *    1. Creates a Scanner object and reads the data from transactions.txt.
+     *    2. Populates an ArrayList with transaction objects.
+     *    3. Sorts the ArrayList.
+     */
+    public static ArrayList<Transaction> returnTransactions() throws FileNotFoundException {
+        ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+        FileInputStream fis = new FileInputStream(TRANSACTIONS_FILE);
+        Scanner scan = new Scanner(fis);
+        while (scan.hasNextLine()) {
+            String[] values = scan.nextLine().split(",");
+            transactions.add(new Transaction(Transaction.Type.valueOf(values[1]), Long.parseLong(values[0]), values[2], Double.parseDouble(values[3])));
+        }
+        Collections.sort(transactions);
+        scan.close();
+        return transactions;
+    }
+
+    /**
+     * Name: runTransactions
+     * @param transactions ArrayList<Transaction>
+     * 
+     * Inside the function:
+     *  1. Executes every transaction using bank.execute.
+     */
+    public static void runTransactions(ArrayList<Transaction> transactions) {
+        transactions.stream().forEach(transaction -> bank.executeTransaction(transaction));
+    }
+
+    /**
+     * Name: transactionHistory
+     * @param id (String)
+     * 
+     * Inside the function
+     *   1. Print: \t\t\t\t   TRANSACTION HISTORY\n\t
+     *   2. Print every transaction that corresponds to the id. (Waits 300 milliseconds before printing the next one)
+     *             - Use this format "\t"+transaction+"\n"
+     *   3. Print: \n\t\t\t\t\tAFTER TAX\n
+     *   4. Print: "\t" + account that corresponds to id +"\n\n\n\n"
+     */
+    public static void transactionHistory(String id) {
+        System.out.println("\t\t\t\t   TRANSACTION HISTORY\n\t");
+        for (Transaction transaction : bank.getTransactions(id)) {
+            wait(300);
+            System.out.println("\t"+ transaction +"\n");      
+        }
+        System.out.println("\n\t\t\t\t\tAFTER TAX\n");
+        System.out.println("\t" + bank.getAccount(id) +"\n\n\n\n");
+    }
 }
