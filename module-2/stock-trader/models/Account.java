e53ac730d4c346ceeb333c39b701bfc95336c4ab
liudmylai <liudmyla.iovenko@gmail.com>
2022-04-07 15:07:49 -0400

    added Trade class

--- module-2/stock-trader/models/Account.java
+++ module-2/stock-trader/models/Account.java
@@ -8,7 +8,7 @@ import models.Trade.Stock;
 public abstract class Account {
 
     private double funds;
-    private HashMap<String, Integer> portfolio;
+    private HashMap<Stock, Integer> portfolio;
 
     public double getFunds() {
         return this.funds;
@@ -18,7 +18,7 @@ public abstract class Account {
         this.funds = funds;
     }
 
-    public Integer getShares(String stock) {
+    public Integer getShares(Stock stock) {
         return this.portfolio.entrySet().stream()
                .filter(entry -> entry.getKey().equals(stock))
                .findFirst()
@@ -26,22 +26,25 @@ public abstract class Account {
                .getValue(); 
     }
 
-    public void setShares(String stock, int shares) {
+    public void setShares(Stock stock, int shares) {
         this.portfolio.put(stock, shares);
     }
 
     public Account(double funds) {
         this.funds = funds;
-        this.portfolio = new HashMap<String, Integer>();
+        this.portfolio = new HashMap<Stock, Integer>();
         this.portfolio.put(Stock.AAPL, 0);
         this.portfolio.put(Stock.FB, 0);
         this.portfolio.put(Stock.GOOG, 0);
         this.portfolio.put(Stock.TSLA, 0); 
     }
-    
+
     public Account(Account source) {
         this.funds = source.funds;
-        this.portfolio = (HashMap<String, Integer>)source.portfolio.entrySet().stream()
+        this.portfolio = (HashMap<Stock, Integer>)source.portfolio.entrySet().stream()
             .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
     }
+
+    public abstract boolean makeTrade(Trade trade);
+
 }
--- module-2/stock-trader/models/Trade.java
+++ module-2/stock-trader/models/Trade.java
@@ -1,10 +1,48 @@
 package models;
 
 public class Trade {
-    static class Stock {
-        public static final String AAPL = "AAPL";
-        public static final String FB = "FB";
-        public static final String GOOG = "GOOG";
-        public static final String TSLA = "TSLA";
+    public enum Stock {AAPL, FB, GOOG, TSLA};
+    public enum Type {MARKET_BUY, MARKET_SELL};
+    private Type type;
+    private Stock stock;
+    private int shares;
+    private double price;
+
+    public double getPrice() {
+        return this.price;
+    }
+    public int getShares() {
+        return this.shares;
+    }
+    public Stock getStock() {
+        return this.stock;
+    }
+    public Type getType() {
+        return this.type;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setShares(int shares) {
+        this.shares = shares;
+    }
+    public void setStock(Stock stock) {
+        this.stock = stock;
+    }
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Trade(Stock stock, Type type, int shares, double price) {
+        this.stock = stock;
+        this.type = type;
+        this.shares = shares;
+        this.price = price;
+    }
+    public Trade(Trade source) {
+        this.stock = source.stock;
+        this.type = source.type;
+        this.shares = source.shares;
+        this.price = source.price;
     }
 }
