55f07e6b6c5f853b30408e3b03dd4fda3cd4cf00
liudmylai <liudmyla.iovenko@gmail.com>
2022-04-16 18:37:33 -0400

    added totalAverage()

--- module-2/global-superstore/Main.java
+++ module-2/global-superstore/Main.java
@@ -1,22 +1,34 @@
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 
 public class Main {
 
-    static final String SALES = "data\\sales.csv"; //Use backslash Windows users
+    static final String SALES = "data\\sales.csv";
 
     public static void main(String[] args) {
         
         try {
             Path path = Paths.get(Thread.currentThread().getContextClassLoader().getResource(SALES).toURI());
-            //calculate average sales of "Furniture" here
-            //calculate average sales of "Technology" here
-            //calculate average sales of "Office Supplies" here
-            //calculate total average of sales here
+            Thread thread2 = new Thread(() -> average(path, "Furniture"));
+            Thread thread3 = new Thread(() -> average(path, "Technology"));
+            Thread thread4 = new Thread(() -> average(path, "Office Supplies"));
+            Thread thread5 = new Thread(() -> totalAverage(path));
+            
+            thread2.start();
+            thread3.start();
+            thread4.start();
+            thread5.start();
 
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Please enter your name to access the Global Superstore dataset: ");
+            String name = scan.nextLine();
+            System.out.println("Access Denied. We apologize for the inconvenience. Have a good day " + name + ".");
+            scan.close();
         } catch (URISyntaxException e) {
             System.out.println(e.getMessage());
         }
@@ -37,22 +49,20 @@ public class Main {
      *   6. Returns the average as double.
      * 
      */
-    public static double average(Path path, String category) {
+    public static Double average(Path path, String category) {
         try {
             return Files.lines(path)
-            .skip(1)
-            .map(line -> line.split(","))
-            .filter(arr -> arr[0].equals(category))
-            .mapToDouble(arr -> Double.parseDouble(arr[1]) * Double.parseDouble(arr[2]))
-            .average()
-            .getAsDouble();
-        } catch (Exception e) {
+                    .skip(1)
+                    .map(line -> line.split(","))
+                    .filter(arr -> arr[0].equals(category))
+                    .mapToDouble(arr -> Double.parseDouble(arr[1]) * Double.parseDouble(arr[2]))
+                    .average()
+                    .getAsDouble();
+        } catch (IOException e) {
             System.out.println(e.getMessage());
-            return 0;
+            return 0.0;
         }
     }
-
-
 
     /**
      * Function name: totalAverage
@@ -66,6 +76,18 @@ public class Main {
      *   4. Applies the terminal operation average.
      *   5. Returns the average as double.
      */
-  
+    public static Double totalAverage(Path path) {
+        try {
+            return Files.lines(path)
+                    .skip(1)
+                    .map(line -> line.split(","))
+                    .mapToDouble(arr -> Double.parseDouble(arr[1]) * Double.parseDouble(arr[2]))
+                    .average()
+                    .getAsDouble();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return 0.0;
+        }
+    }
 
 }
